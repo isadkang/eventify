7f37b591eafb74423a55f4da5ff88eb9be4f4418
isadkang <irsyadrecovery15@gmail.com>
2025-08-31 20:10:53 +0700

    fix env

--- config/db.go
+++ config/db.go
@@ -14,11 +14,11 @@ var DB *pgxpool.Pool
 func ConnectDB() error {
 	dsn := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGNAME"),
 	)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
